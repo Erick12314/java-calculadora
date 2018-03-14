4752f977f52d9ce312d3a17e5eca1abe48644c3f
ERICK-GITHUB <erick3797@hotmail.com>
2018-03-14 03:45:22 -0500

    Vistas agregadas y logica del controlador

--- ProyCalculadora/src/main/java/com/edu/calculadora/model/Calculadora.java
+++ ProyCalculadora/src/main/java/com/edu/calculadora/model/Calculadora.java
@@ -2,4 +2,34 @@ package com.edu.calculadora.model;
 
 public class Calculadora {
 
+	private double numero1;
+	private double numero2;
+	private char operacion;
+	private double resultado;
+	
+	public double getNumero1() {
+		return numero1;
+	}
+	public void setNumero1(double numero1) {
+		this.numero1 = numero1;
+	}
+	public double getNumero2() {
+		return numero2;
+	}
+	public void setNumero2(double numero2) {
+		this.numero2 = numero2;
+	}
+	public char getOperacion() {
+		return operacion;
+	}
+	public void setOperacion(char operacion) {
+		this.operacion = operacion;
+	}
+	public double getResultado() {
+		return resultado;
+	}
+	public void setResultado(double resultado) {
+		this.resultado = resultado;
+	}
+	
 }
--- ProyCalculadora/src/main/java/com/edu/calculadora/web/CalculadoraController.java
+++ ProyCalculadora/src/main/java/com/edu/calculadora/web/CalculadoraController.java
@@ -1,23 +1,60 @@
 package com.edu.calculadora.web;
 
+import com.edu.calculadora.model.Calculadora;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class CalculadoraController {
-	
+
 	@GetMapping("/calculadora")
-	private String calculadoraForm() {
-		
-		
-		return null;
+	private String calculadoraForm(Model model) {
+
+		model.addAttribute("calculadora", new Calculadora());
+
+		return "calcForm";
 	}
-	
+
 	@PostMapping("/resultado")
-	private String resultadoForm() {
+	private String resultadoForm(@ModelAttribute Calculadora calculadora) {
 		
+		double num1 = calculadora.getNumero1();
+		double num2 = calculadora.getNumero2();
 		
-		return null;
+		switch (calculadora.getOperacion()) {
+		case '+':
+			calculadora.setResultado(num1 + num2);
+			break;
+		case '-':
+			calculadora.setResultado(num1 - num2);
+			break;
+		case '*':
+			calculadora.setResultado(num1 * num2);
+			break;
+		case '/':
+			
+			//NO SE PUEDE DIVIDIR UN NUMERO ENTRE CERO
+			if (num2 != 0) {
+				
+				calculadora.setResultado(num1 / num2);
+				
+			}else {
+				
+				
+				
+			}
+			
+			break;
+		default:
+			
+			
+			
+			break;
+		}
+
+		return "result";
 	}
 }
